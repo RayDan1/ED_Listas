b46245828b6d5a716f9f6067f875e2f148f9d631
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2022-04-09 13:48:49 -0700

    Nodo Busqueda

--- ListasSimplementeLigadas/Lista.cs
+++ ListasSimplementeLigadas/Lista.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace ListasSimplementeLigadas
 {
-    internal class Lista
+    public class Lista
     {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            return (nodoInicial.Enlace == null);
+            
+        }
+        public void Vaciar()
+        {
+            nodoInicial.Enlace = null;
+        }
+        public string RecorrerLista()
+        {
+            string datosLista = "";
+            nodoActual = nodoInicial;
+            while(nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+                datosLista += $"{nodoActual.Valor}\n";
+            }
+            return datosLista;
+        }
+        public void AgregarNodo(string dato)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+            }
+            Nodo nodoNuevo = new Nodo(dato);
+            nodoActual.Enlace = nodoNuevo;
+        }
+        public Nodo Buscar(string dato)
+        {
+            if(ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace = null)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    if(nodoBusqueda.Valor == dato)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
