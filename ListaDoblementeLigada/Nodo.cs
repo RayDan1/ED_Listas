4cbb8cb0d04dd9840a23f44330937624e2503cb4
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2022-04-23 13:54:11 -0700

    agregamos las clases nodo y litas en el proyecto "ListaDoblementeLigado"

---
+++ ListaDoblementeLigada/Nodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDoblementeLigada
+{
+    public class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo nodoAnterior { get; set; }
+        public Nodo nodoSiguiente { get; set; }
+    }
+}
